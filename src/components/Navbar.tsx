4f2b589302984bd1ad09a9dfb0af65d0a7262e0b
Efe G <98314362+gift56@users.noreply.github.com>
2024-10-01 18:47:11 -0500

    working on next social headers

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,17 @@
 import Link from "next/link";
+import MobileMenu from "./MobileMenu";
 
 const Navbar = () => {
   return (
     <nav>
       <div>
-        <Link href="/">SocialMedia</Link>
+        <Link href="/" className="uppercase">
+          SocialMedia
+        </Link>
+      </div>
+      <div></div>
+      <div>
+        <MobileMenu />
       </div>
     </nav>
   );
