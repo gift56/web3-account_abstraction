ef93b7357f21fc7b32b88b06ba57542e96ef239f
Efe G <98314362+gift56@users.noreply.github.com>
2024-10-08 16:23:50 -0500

    woking on mobile and desktop navbar

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -1,7 +1,45 @@
 "use client";
 
+import Link from "next/link";
+import { useState } from "react";
+
 const MobileMenu = () => {
-  return <div>MobileMenu</div>;
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="md:hidden">
+      <button
+        type="button"
+        className="flex flex-col gap-1"
+        onClick={() => setIsOpen((prev) => !prev)}
+      >
+        <div
+          className={`w-6 h-1 bg-blue-500 rounded-sm origin-left ease-in-out duration-500 ${
+            isOpen ? "rotate-45" : ""
+          }`}
+        />
+        <div
+          className={`w-6 h-1 bg-blue-500 rounded-sm ease-in-out duration-500 ${
+            isOpen ? "opacity-0" : ""
+          }`}
+        />
+        <div
+          className={`w-6 h-1 bg-blue-500 rounded-sm origin-left ease-in-out duration-500 ${
+            isOpen ? "-rotate-45" : ""
+          }`}
+        />
+      </button>
+      {isOpen && (
+        <div className="w-full  h-[calc(100vh-96px)] bg-white flex flex-col items-center justify-center gap-8 font-medium text-xl z-10 absolute left-0 top-24">
+          <Link href="/">Home</Link>
+          <Link href="/">Friends</Link>
+          <Link href="/">Groups</Link>
+          <Link href="/">Stories</Link>
+          <Link href="/">Login</Link>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default MobileMenu;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,32 @@
 import Link from "next/link";
 import MobileMenu from "./MobileMenu";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
-    <nav>
-      <div>
-        <Link href="/" className="uppercase">
+    <nav className="flex items-center justify-between w-full h-24">
+      <div className="md:hidden lg:block w-[20%]">
+        <Link href="/" className="uppercase text-xl font-bold text-blue-600">
           SocialMedia
         </Link>
       </div>
-      <div></div>
-      <div>
+      <div className="hidden md:flex w-[50%] text-sm">
+        <div className="flex gap-6 text-gray-600">
+          <Link href="/" className="flex gap-2 items-center justify-start">
+            <Image src="/home.png" alt="Homepage" width={16} height={16} />
+            <span>Homepage</span>
+          </Link>
+          <Link href="/" className="flex gap-2 items-center justify-start">
+            <Image src="/friends.png" alt="Friends" width={16} height={16} />
+            <span>Friends</span>
+          </Link>
+          <Link href="/" className="flex gap-2 items-center justify-start">
+            <Image src="/stories.png" alt="Stories" width={16} height={16} />
+            <span>Stories</span>
+          </Link>
+        </div>
+      </div>
+      <div className="w-[30%] flex items-center gap-4 xl:gap-8 justify-end">
         <MobileMenu />
       </div>
     </nav>
